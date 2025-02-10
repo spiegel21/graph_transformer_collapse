22b6ca9e36c773c60444b048dabd9530cb5fa7b3
Eduardo Spiegel21 <eduardospiegel@MacBook-Pro-de-Eduardo.local>
2025-02-09 23:34:24 -0800

    added gin

--- run_gnn.sh
+++ run_gnn.sh
@@ -17,6 +17,14 @@ rm -rf models out
 python main.py $FILE
 done
 
+# # GIN
+# for FILE in configs/final/gin/C_2/*
+# do
+# echo -e "$FILE"
+# rm -rf models out
+# python main.py $FILE
+# done
+
 # GCN with the same parameters (few layers, tiny hidden size)
 # for FILE in configs/final/sample/T1/*
 # do
