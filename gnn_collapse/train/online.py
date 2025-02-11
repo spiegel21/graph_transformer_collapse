bc0c91be13927800ad9b28cd135800fb080985cb
Eduardo Spiegel21 <eduardospiegel@MacBook-Pro-de-Eduardo.local>
2025-02-11 10:31:22 -0800

    added gradient clipping

--- gnn_collapse/train/online.py
+++ gnn_collapse/train/online.py
@@ -133,6 +133,8 @@ class OnlineRunner:
         loss = compute_loss_multiclass(type=self.args["loss_type"], pred=pred, labels=data.y, C=self.args["C"])
         model.zero_grad()
         loss.backward()
+        # might add gradient clipping here
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
         acc = compute_accuracy_multiclass(pred=pred, labels=data.y, C=self.args["C"])
         return model, optimizer, loss.detach().cpu().numpy(), acc
