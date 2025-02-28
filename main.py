aa8a49772c62873ef90d9dabe891d9bc1207dd61
Eduardo Spiegel21 <eduardospiegel@MacBook-Pro-de-Eduardo.local>
2025-02-27 21:22:51 -0600

    time to run

--- main.py
+++ main.py
@@ -100,95 +100,54 @@ if __name__ == "__main__":
         n_embedding = 0
         transform = None
     if args["model_name"] not in ["bethe_hessian", "normalized_laplacian"]:
-        # Construct directory names based on the parameters for caching the datasets.
-        train_dirname = "N_{}_C_{}_Pr_{}_p_{}_q_{}_num_graphs_{}_feat_strat_{}_feat_dim_{}_permute_{}".format(
-            args["N_train"],
-            args["C"],
-            args["Pr"],
-            args["p_train"],
-            args["q_train"],
-            args["num_train_graphs"],
-            args["feature_strategy"],
-            args["input_feature_dim"],
-            args.get("permute", True)
+        train_sbm_dataset = SBM(
+            args=args,
+            N=args["N_train"],
+            C=args["C"],
+            Pr=args["Pr"],
+            p=args["p_train"],
+            q=args["q_train"],
+            num_graphs=args["num_train_graphs"],
+            feature_strategy=args["feature_strategy"],
+            feature_dim=args["input_feature_dim"],
+            is_training=True,
+            transform=transform
         )
-        nc_dirname = train_dirname + "_nc"  # differentiate the nc dataset if needed
-        test_dirname = "N_{}_C_{}_Pr_{}_p_{}_q_{}_num_graphs_{}_feat_strat_{}_feat_dim_{}_permute_{}".format(
-            args.get("N_test", args.get("N")),
-            args["C"],
-            args["Pr"],
-            args.get("p_test", args.get("p")),
-            args.get("q_test", args.get("q")),
-            args["num_test_graphs"],
-            args["feature_strategy"],
-            args["input_feature_dim"],
-            args.get("permute", True)
+        nc_sbm_dataset = SBM(
+            args=args,
+            N=args["N_train"],
+            C=args["C"],
+            Pr=args["Pr"],
+            p=args["p_train"],
+            q=args["q_train"],
+            num_graphs=args["num_train_graphs"],
+            feature_strategy=args["feature_strategy"],
+            feature_dim=args["input_feature_dim"],
+            is_training=True,
+            transform=transform
         )
-
-        # Directories where datasets are (or will be) stored.
-        train_path = os.path.join("data", "sbm", "train", train_dirname)
-        nc_path = os.path.join("data", "sbm", "train", nc_dirname)
-        test_path = os.path.join("data", "sbm", "test", test_dirname)
-
-        # Helper function to load or create dataset.
-        def get_dataset(cache_path, dataset_params):
-            dataset_file = os.path.join(cache_path, "dataset.pt")
-            # if os.path.exists(dataset_file):
-            #     print("Loading dataset from", dataset_file)
-            #     dataset = torch.load(dataset_file)
-            # else:
-            #     print("Creating dataset and saving to", dataset_file)
-            #     dataset = SBM(**dataset_params)
-            #     os.makedirs(cache_path, exist_ok=True)
-            #     torch.save(dataset, dataset_file)
-            print("Creating dataset and saving to", dataset_file)
-            dataset = SBM(**dataset_params)
-            os.makedirs(cache_path, exist_ok=True)
-            torch.save(dataset, dataset_file)
-            return dataset
-
-        # Prepare parameters for the SBM dataset instantiation.
-        train_params = {
-            "args": args,
-            "N": args["N_train"],
-            "C": args["C"],
-            "Pr": args["Pr"],
-            "p": args["p_train"],
-            "q": args["q_train"],
-            "num_graphs": args["num_train_graphs"],
-            "feature_strategy": args["feature_strategy"],
-            "feature_dim": args["input_feature_dim"],
-            "is_training": True,
-            "transform": transform
-        }
-
-        # For nc dataset, we use the same parameters.
-        nc_params = dict(train_params)
-
-        test_params = {
-            "args": args,
-            "N": args.get("N_test", args.get("N")),
-            "C": args["C"],
-            "Pr": args["Pr"],
-            "p": args.get("p_test", args.get("p")),
-            "q": args.get("q_test", args.get("q")),
-            "num_graphs": args["num_test_graphs"],
-            "feature_strategy": args["feature_strategy"],
-            "feature_dim": args["input_feature_dim"],
-            "is_training": False,
-            "transform": transform
-        }
-
-        # Get or create datasets.
-        train_sbm_dataset = get_dataset(train_path, train_params)
-        nc_sbm_dataset = get_dataset(nc_path, nc_params)
-        test_sbm_dataset = get_dataset(test_path, test_params)
-
         # keep batch size = 1 for consistent measurement of loss and accuracies under
         # permutation of classes.
         train_dataloader = DataLoader(dataset=train_sbm_dataset, batch_size=1)
         nc_dataloader = DataLoader(dataset=nc_sbm_dataset, batch_size=1)
+
+        test_sbm_dataset = SBM(
+            args=args,
+            N=args.get("N_test", args.get("N")),
+            C=args["C"],
+            Pr=args["Pr"],
+            p=args.get("p_test", args.get("p")),
+            q=args.get("q_test", args.get("q")),
+            num_graphs=args["num_test_graphs"],
+            feature_strategy=args["feature_strategy"],
+            feature_dim=args["input_feature_dim"],
+            is_training=False,
+            transform=transform
+        )
         test_dataloader = DataLoader(dataset=test_sbm_dataset, batch_size=1)
+
+
+        # print(args['input_feature_dim'])
 
         args['input_feature_dim'] += n_embedding
     
