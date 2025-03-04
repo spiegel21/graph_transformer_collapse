ff81823ff18b949058f3a51f8e0b088866bd3991
Eduardo Spiegel21 <eduardospiegel@MacBook-Pro-de-Eduardo.local>
2025-03-04 10:20:26 -0600

    added more epochs and graphs to smpnn

--- run_gnn.sh
+++ run_gnn.sh
@@ -26,13 +26,13 @@ echo -e "$FILE"
 python main.py $FILE
 done
 
-# Graph Transformer
-for FILE in configs/final/easygt/C_2/*
-do
-echo -e "$FILE"
-# rm -rf models out
-python main.py $FILE
-done
+# # Graph Transformer
+# for FILE in configs/final/easygt/C_2/*
+# do
+# echo -e "$FILE"
+# # rm -rf models out
+# python main.py $FILE
+# done
 
 # GCN with the same parameters (few layers, tiny hidden size)
 # for FILE in configs/final/sample/T1/*
