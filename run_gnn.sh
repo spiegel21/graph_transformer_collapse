4eac1f7f1787789e186bcee92f4a52410cef179f
Eduardo Spiegel21 <eduardospiegel@MBP-de-Eduardo.lan>
2025-03-06 15:28:44 -0600

    last run

--- run_gnn.sh
+++ run_gnn.sh
@@ -2,61 +2,34 @@
 
 # mkdir -p out/final/
 
-# # GIN
-# for FILE in configs/final/gin/C_2/*
-# do
-# echo -e "$FILE"
-# # rm -rf models out
-# python main.py $FILE
-# done
-
-# # GCN
-# for FILE in configs/final/graphconv/C_2/N_1000_C_2_p_0.025_q_0.0017_Ktrain_1000_Ktest_100_L_32_fs_rn_opt_sgd_use_W1_false.json
-# do
-# echo -e "$FILE"
-# # rm -rf models out
-# python main.py $FILE
-# done
-
-# SMPNN
-for FILE in configs/final/smpnn/C_2/*
+# GIN
+for FILE in configs/final/gin/C_2/*
 do
 echo -e "$FILE"
 # rm -rf models out
 python main.py $FILE
 done
 
-# # Graph Transformer
-# for FILE in configs/final/easygt/C_2/*
+# GCN
+for FILE in configs/final/graphconv/C_2/N_1000_C_2_p_0.025_q_0.0017_Ktrain_1000_Ktest_100_L_32_fs_rn_opt_sgd_use_W1_false.json
+do
+echo -e "$FILE"
+# rm -rf models out
+python main.py $FILE
+done
+
+# # SMPNN
+# for FILE in configs/final/smpnn/C_2/*
 # do
 # echo -e "$FILE"
 # # rm -rf models out
 # python main.py $FILE
 # done
 
-# GCN with the same parameters (few layers, tiny hidden size)
-# for FILE in configs/final/sample/T1/*
-# do
-# echo -e "$FILE"
+# Graph Transformer
+for FILE in configs/final/easygt/C_2/*
+do
+echo -e "$FILE"
 # rm -rf models out
-# python main.py $FILE
-# done
-
-# for FILE in configs/final/graphconv/C_4/*
-# do
-# echo -e "$FILE"
-# python main.py $FILE
-# done
-
-
-# for FILE in configs/final/graphconv_hetero/C_2/*
-# do
-# echo -e "$FILE"
-# python main.py $FILE
-# done
-
-# for FILE in configs/final/graphconv_hetero/C_4/*
-# do
-# echo -e "$FILE"
-# python main.py $FILE
-# done
+python main.py $FILE
+done
